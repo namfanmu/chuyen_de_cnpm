55cee66461b43b9ca22505be015c85d7f44a3969
Nguyễn Bá Nam <nguyenbanam.281999@gmail.com>
2021-04-28 23:01:53 +0700

    Chuyen de CNPM nhom 8

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+import FlutterMacOS
+import Foundation
+
+import package_info
+import path_provider_macos
+import url_launcher_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+}
